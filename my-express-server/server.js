c9fed2debaf0ecc746491c0f904a42c50d25796f
hiden52 <zerorequiem23@gmail.com>
2021-01-30 04:44:50 +0000

    Learn about send resopnse

---
+++ my-express-server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>hello, World!</h1>");
+});
+app.get("/contact", (req, res) => {
+    res.send("<h2>Contact me</h2>")
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h2>I'm a Developer</h2><ul><li>JAVASCRIPT</li><li>C++</li></ul>");
+});
+
+app.listen(port, () => {
+    console.log("Server started on port 3000");
+});
