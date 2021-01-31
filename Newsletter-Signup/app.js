8645e336e5852c70ae07ecaa7b1ee2c90d549aca
hiden52 <zerorequiem23@gmail.com>
2021-01-31 23:39:49 +0900

    using mailChimp client library

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-const mailchimp = require("@mailchimp/mailchimp_marketing");
+const mailChimp = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,39 +17,68 @@ app.post("/", (req, res) => {
 	const firstName = req.body.fName;
 	const lastName = req.body.lName;
 	const email = req.body.email;
-	console.log(firstName, lastName, email);
+	//console.log(firstName, lastName, email);
 
-	const data = {
-		members: [
-			{
-				email_address: email,
-				status: "subscribed",
-				merge_fields: {
-					FNAME: firstName,
-					LNAME: lastName,
-				},
-			},
-		],
+	// const data = {
+	// 	members: [
+	// 		{
+	// 			email_address: email,
+	// 			status: "subscribed",
+	// 			merge_fields: {
+	// 				FNAME: firstName,
+	// 				LNAME: lastName,
+	// 			},
+	// 		},
+	// 	],
+	// };
+
+	//const jsonData = JSON.stringify(data);
+
+	//const url = "https://us7.api.mailchimp.com/3.0/lists/" + listId;
+
+	// const options = {
+	// 	method: "POST",
+	// 	auth: "saigony:" + mailChimpApiKey,
+	// };
+
+	// --without mailchimp client library--
+	// const request = https.request(url, options, (response) => {
+	// 	response.on("data", (data) => {
+	// 		console.log(JSON.parse(data));
+	// 	});
+	// });
+	// request.write(jsonData);
+	// request.end();
+
+	const subscribeingUser = {
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
 	};
-
-	const jsonData = JSON.stringify(data);
-
-	const mailChimpApiKey = "70a60a5e20f865b92c4ea129ef5ef24c-us7";
+	// Setting api key and server
+	
 	const listId = "e991bcecf8";
-	const url = "https://us7.api.mailchimp.com/3.0/lists/" + listId;
-	const options = {
-		method: "POST",
-		auth: "saigony:" + mailChimpApiKey,
+
+	const run = async () => {
+		const response = await mailChimp.lists.addListMember(listId, {
+			email_address: subscribeingUser.email,
+			status: "subscribed",
+			merge_fields: {
+				FNAME: subscribeingUser.firstName,
+				LNAME: subscribeingUser.lastName,
+			},
+		});
+
+		//console.log(response);
 	};
 
-	const request = https.request(url, options, (response) => {
-		response.on("data", (data) => {
-			console.log(JSON.parse(data));
-		});
-	});
-    
-    request.write(jsonData);
-    request.end();
+	run();
+});
+
+const mailChimpApiKey = "70a60a5e20f865b92c4ea129ef5ef24c-us7";
+mailChimp.setConfig({
+	apiKey: mailChimpApiKey,
+	server: "us7",
 });
 
 app.listen(port, () => {
