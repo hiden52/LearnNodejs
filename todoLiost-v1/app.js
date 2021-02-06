f5e1db39adb187e713e154787cbb2d5cdc192c1d
hiden52 <zerorequiem23@gmail.com>
2021-02-06 14:49:28 +0000

    Can add new todo list

--- todoLiost-v1/app.js
+++ todoLiost-v1/app.js
@@ -3,40 +3,56 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
+let items = ["Coding", "Learn JS", "Master React"];
 
 app.get("/", (req, res) => {
   const today = new Date();
   const currentDay = today.getDay();
-  let day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wendsday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
-  res.render("list", { kindOfDay: day });
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  const day = today.toLocaleDateString("en-US", options);
+
+  //   switch (currentDay) {
+  //     case 0:
+  //       day = "Sunday";
+  //       break;
+  //     case 1:
+  //       day = "Monday";
+  //       break;
+  //     case 2:
+  //       day = "Tuesday";
+  //       break;
+  //     case 3:
+  //       day = "Wendsday";
+  //       break;
+  //     case 4:
+  //       day = "Thursday";
+  //       break;
+  //     case 5:
+  //       day = "Friday";
+  //       break;
+  //     case 6:
+  //       day = "Saturday";
+  //       break;
+  //     default:
+  //       console.log("Error: current day is equal to: " + currentDay);
+  //   }
+  res.render("list", { kindOfDay: day, newTasks: items });
+});
+
+app.post("/", (req, res) => {
+  item = req.body.task;
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
