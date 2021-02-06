42e894671a0960101bca32f1de22f1469293e942
hiden52 <zerorequiem23@gmail.com>
2021-02-06 14:09:18 +0000

    Use EJS

--- todoLiost-v1/app.js
+++ todoLiost-v1/app.js
@@ -2,12 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
+
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("hello");
+  const today = new Date();
+  const currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wendsday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+  }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(port, () => {
-    console.log("Server started on port " + port);
+  console.log("Server started on port " + port);
 });
