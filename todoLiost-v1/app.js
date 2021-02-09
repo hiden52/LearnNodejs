561a17a2ba10ad4bc14957092ef3b44571935df7
hiden52 <zerorequiem23@gmail.com>
2021-02-09 14:49:22 +0000

    Upgrade 'todolist-v1' link mongoDB using mongoose.

--- todoLiost-v1/app.js
+++ todoLiost-v1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -8,7 +9,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const port = 3000;
 
-const items = ["Coding", "Learn JS", "Master React"];
+//const items = ["Coding", "Learn JS", "Master React"];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+const taskSchema = {
+    name: {
+        type: String,
+        required: true,
+    },
+};
+const Task = mongoose.model("Task", taskSchema);
+
+const coding = new Task({
+    name: "Code all day!",
+});
+const study = new Task({ name: "Study" });
+const buySnacks = new Task({ name: "Buy some snacks" });
+
+const defaultTasks = [coding, study, buySnacks];
 
 app.get("/", (req, res) => {
     // not good
@@ -38,12 +59,40 @@ app.get("/", (req, res) => {
     //       console.log("Error: current day is equal to: " + currentDay);
     //   }
     const day = date.getDate();
-    res.render("list", { kindOfDay: day, tasks: items });
+
+    Task.find({}, (err, tasks) => {
+        if (tasks.length === 0) {
+            Task.insertMany(defaultTasks, (err) => {
+                if (err) console.log(err);
+                else {
+                    console.log("Successfully insert docs!");
+                    res.redirect("/");
+                }
+            });
+        } else res.render("list", { kindOfDay: day, tasks: tasks });
+    });
 });
 
 app.post("/", (req, res) => {
-    const item = req.body.task;
-    items.push(item);
+    // const item = req.body.task;
+    // items.push(item);
+    const newtask = new Task({ name: req.body.task });
+    newtask.save();
+
+    res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const checkedTaskId = req.body.checkbox;
+    if (checkedTaskId) {
+        Task.findByIdAndDelete(checkedTaskId, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Successfully deleted the document!");
+            }
+        });
+    }
 
     res.redirect("/");
 });
