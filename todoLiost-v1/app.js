6df8c332a414c97e2110d7bdc0501872fd50be60
hiden52 <zerorequiem23@gmail.com>
2021-02-10 11:55:02 +0000

    Cehck app.js line 76 !! BUG!!!!

--- todoLiost-v1/app.js
+++ todoLiost-v1/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const port = 3000;
 
-//const items = ["Coding", "Learn JS", "Master React"];
+const items = ["Coding", "Learn JS", "Master React"];
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -73,6 +73,28 @@ app.get("/", (req, res) => {
     });
 });
 
+// 루트페이지로 들어가면 버그발생 ㅜㅜ 고치자!
+app.get("/:categoryName", (req, res) => {
+    const category = req.params.categoryName;
+
+    const collections = mongoose.modelNames();
+    collections.forEach((col, i) => {
+        if(col === category) {
+            console.log("It name of collection exists!");
+            const NewCategory = mongoose.model(category);
+        }
+        if(i === collections.length) {const NewCategory = mongoose.model(category, taskSchema);}
+
+    });
+    
+
+    NewCategory.find({}, (err, tasks) =>{
+        res.render("list", {kindOfDay: category, tasks: tasks});
+    });
+    console.log(mongoose.modelNames());
+
+});
+
 app.post("/", (req, res) => {
     // const item = req.body.task;
     // items.push(item);
@@ -80,6 +102,14 @@ app.post("/", (req, res) => {
     newtask.save();
 
     res.redirect("/");
+});
+app.post("/:categoryName", (req, res) => {
+    const category = req.params.categoryName;
+    const NewCategory = mongoose.model(category);
+    const newtask = new NewCategory({ name: req.body.task });
+    newtask.save();
+
+    res.redirect("/" + category);
 });
 
 app.post("/delete", (req, res) => {
