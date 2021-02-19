cc52849cd43ef0f12a96cfd1153303a0ef2cbde6
hiden52 <zerorequiem23@gmail.com>
2021-02-19 14:35:56 +0000

    routes chaining in express. GET, POST, DELETE

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -15,17 +15,75 @@ app.use(
 );
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost:27017/wikiDB", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Successfully connected to mongodb"))
+  .catch(console.error);
 
 const articleSchema = new mongoose.Schema({
-    title: String,
-    Content: String
+  title: String,
+  content: String,
 });
 
-const Article = new mongoose.model("articles", articleSchema);
+const Article = mongoose.model("articles", articleSchema);
+
+const coding = new Article({
+  title: "Coding",
+  content: "Coding is so fun!",
+});
+//coding.save();
+
+app.get("/", (req, res) => {
+  res.render("post", {});
+});
+
+
+///////////////////////////////// Request Targetting All Articles
+app
+  .route("/article") //
+  .get((req, res) => {
+    Article.find({}, (err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save();
+
+    res.redirect("/article");
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err) {
+        res.send("Deleted All Articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+
+  /////////////////////////// Request Targetting A Specific Article //
+  app
+  .route("/article/:title") //
+  .get((req, res) => {
+    Article.findOne({ title: req.params.title }, (err, foundArticle) => {
+      if (foundArticle) res.send(foundArticle);
+      else res.send("Not Found");
+    });
+  });
 
 
 
